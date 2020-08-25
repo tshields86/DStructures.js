2933ca509059e6e0bacb2484b1b4d82f216aba66
Travis Shields <travis.shields@gmail.com>
2020-08-25 13:00:46 -0400

    Add index file for exports

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* data structures */
+const LinkedList = require('./data-structures/linked-list/LinkedList');
+const DoublyLinkedList = require('./data-structures/doubly-linked-list/DoublyLinkedList');
+
+/* algorithms */
+
+module.exports = {
+  LinkedList,
+  DoublyLinkedList,
+};
