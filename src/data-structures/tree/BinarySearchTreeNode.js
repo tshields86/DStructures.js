8cec3d0395c13eaaa6c5762f3856054380f57da7
Travis Shields <travis.shields@gmail.com>
2020-09-17 14:09:43 -0400

    Add BinarySearchTreeNode and tests

---
+++ src/data-structures/tree/BinarySearchTreeNode.js
@@ -0,0 +1,125 @@
+const BinaryTreeNode = require('./BinaryTreeNode');
+
+/**
+ * Binary Search Tree Node
+ */
+class BinarySearchTreeNode extends BinaryTreeNode {
+  /**
+   * @param {*} [value]
+   */
+  constructor(value = null) {
+    super(value);
+  }
+
+  /**
+   * @param {*} value
+   * @return {BinarySearchTreeNode}
+   */
+  insert(value) {
+    if (this.value === null) {
+      this.value = value;
+
+      return this;
+    }
+
+    if (value < this.value) {
+      if (this.left) return this.left.insert(value);
+
+      const newNode = new BinarySearchTreeNode(value);
+      this.setLeft(newNode);
+
+      return newNode;
+    }
+
+    if (value > this.value) {
+      if (this.right) return this.right.insert(value);
+
+      const newNode = new BinarySearchTreeNode(value);
+      this.setRight(newNode);
+
+      return newNode;
+    }
+
+    this.meta.multiplicity = (this.meta.multiplicity || 1) + 1;
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {boolean}
+   */
+  remove(value) {
+    const nodeToRemove = this.find(value);
+
+    if (!nodeToRemove) return false;
+
+    if (nodeToRemove.meta.multiplicity && nodeToRemove.meta.multiplicity > 1) {
+      nodeToRemove.meta.multiplicity--;
+      return true;
+    }
+
+    const { parent } = nodeToRemove;
+
+    if (!nodeToRemove.left && !nodeToRemove.right) {
+      if (parent) parent.removeChild(nodeToRemove);
+      else nodeToRemove.setValue(null);
+    } else if (nodeToRemove.left && nodeToRemove.right) {
+      const nextBiggerNode = nodeToRemove.right.findMin();
+
+      if (nextBiggerNode !== nodeToRemove.right) {
+        this.remove(nextBiggerNode.value);
+        nodeToRemove.setValue(nextBiggerNode.value);
+      } else {
+        nodeToRemove.setValue(nodeToRemove.right.value);
+        nodeToRemove.setRight(nodeToRemove.right.right);
+      }
+    } else {
+      const childNode = nodeToRemove.left || nodeToRemove.right;
+
+      if (parent) parent.replaceChild(nodeToRemove, childNode);
+      else BinaryTreeNode.copyNode(childNode, nodeToRemove);
+    }
+
+    return true;
+  }
+
+  /**
+   * @return {BinarySearchTreeNode}
+   */
+  findMin() {
+    if (!this.left) return this;
+
+    return this.left.findMin();
+  }
+
+  /**
+   * @param {*} value
+   * @return {(BinarySearchTreeNode|null)}
+   */
+  find(value) {
+    if (this.value === value) {
+      return this;
+    }
+
+    if (value < this.value && this.left) {
+      return this.left.find(value);
+    }
+
+    if (value > this.value && this.right) {
+      return this.right.find(value);
+    }
+
+    return null;
+  }
+
+  /**
+   * @param {*} value
+   * @return {boolean}
+   */
+  contains(value) {
+    return !!this.find(value);
+  }
+}
+
+module.exports = BinarySearchTreeNode;
